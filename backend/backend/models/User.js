a3f96390d02e5739d01b240729932e558846a57f
rahmat <shomare.majazibijan@gmail.com>
2025-12-12 18:43:31 +0330

    Create User.js

---
+++ backend/backend/models/User.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+    role: { type: String, default: "user" }
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (pass) {
+  return await bcrypt.compare(pass, this.password);
+};
+
+export default mongoose.model("User", userSchema);
